9e716c7471cc21d3b9ca5932ea139eff754b347d
buleks <>
2017-06-16 22:38:09 +0000

    Script to get new data works

--- data_test.py
+++ data_test.py
@@ -3,6 +3,8 @@ import calendar
 import datetime
 import os
 import sys
+import requests
+import json
 
 calendar.setfirstweekday(calendar.MONDAY)
 
@@ -13,14 +15,18 @@ TODAY = datetime.date.today()
 missingFiles = []
 
 
+def filename(date):
+    return f'{date.year}{date.month:02}{date.day:02}'
+
+
 def checkData():
     date = FIRST
     while date < TODAY:
-        file = f'{date.year}{date.month:02}{date.day:02}.json'
         date = date+datetime.timedelta(days=1)
-        if not pathlib.Path(DATA_DIR + file).is_file():
+        file = filename(date)
+        if not pathlib.Path(DATA_DIR + file+".json").is_file():
             print(file, '-missing')
-            missingFiles.append(file)
+            missingFiles.append(date)
 
 
 def getMissingData():
@@ -30,6 +36,19 @@ def getMissingData():
     else:
         print("APIKEY not found")
         sys.exit(1)
+    for x in missingFiles:
+        date = filename(x)
+        query_link = f'http://api.wunderground.com/api/{API_KEY}/history_{date}/q/zmw:00000.1.12566.json'
+        request = requests.get(query_link)
+        data = request.json()
+        try:
+            file = DATA_DIR+date+".json"
+            out = open(file, 'w')
+            json.dump(data, out)
+            out.close()
+            print("File created:"+file)
+        except:
+            print("Unable to write file:"+date);
 
 if __name__ == '__main__':
     checkData()
