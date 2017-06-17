681e3bfd8b61728b3ce52e8ad573fec2647a2c78
buleks <>
2017-06-17 00:40:42 +0000

    Simple script to show data form json scripts

---
+++ data_view.py
@@ -0,0 +1,52 @@
+import datetime
+import os
+import json
+import matplotlib.pyplot as plt
+
+
+def test1():
+    file_date = datetime.date(2014, 6, 7)
+    data = loadDay(file_date)
+    observations = data['history']['observations']
+    for obs in observations:
+        temp = obs['tempm']
+        print(obs['date']['pretty']+" Temp:"+temp)
+
+
+def tempDayData(date):
+    data = loadDay(date)
+    observations = data['history']['observations']
+    temp = []
+    for obs in observations:
+        temp.append(float(obs['tempm']))
+    return temp;
+def test2(day,prevdays):
+    first = day - +datetime.timedelta(days=prevdays)
+    last = day
+    i = first
+    legendString=[]
+    while i <= last:
+        plt.plot(tempDayData(i))
+        legendString.append(dateString(i))
+        i = i +datetime.timedelta(days=1)
+    plt.legend(legendString,loc='upper left')
+    plt.show()
+
+
+
+def dateString(date):
+    return f'{date.year}{date.month:02}{date.day:02}'
+
+
+def loadDay(file_date):
+    path = "data/"+dateString(file_date) + '.json'
+    if os.path.isfile(path):
+        try:
+            res_data = json.load(open(path))
+            return res_data
+
+        except:
+            print("Can not read file:"+path)
+            return None
+
+test2(datetime.date(2017, 6, 16), 7)
